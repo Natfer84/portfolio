715bc09b53e9d2de7c3d14b719c44db81f44ba3b
Natfer84 <natita2804@hotmail.com>
2025-04-13 12:02:17 +0200

    responsi

--- front/src/components/Nav.js
+++ front/src/components/Nav.js
@@ -6,7 +6,6 @@ export default function Nav() {
   // Para cambiar el estado del menú, abierto o cerrado
   const [menuOpen, setMenuOpen] = useState(false);
 
-
   // Para realizar el efecto de mostrar el menú normal en tamaño ordenador
   useEffect(() => {
     const menuDesk = () => {
@@ -20,29 +19,34 @@ export default function Nav() {
   }, []);
 
   return (
+    
     <div className={`Container_Nav ${menuOpen ? "Open" : ""}`}>
-      <nav className="Box__Nav">
-        <ul className="Box__Nav__Ul">
-          <li className="Box__Nav__Li">
-            <Link to="/Natalia" onClick={() => setMenuOpen(false)}>NATALIA</Link>
-          </li>
-          <li className="Box__Nav__Li">
-            <Link to="/AboutMe" onClick={() => setMenuOpen(false)}>SOBRE MI</Link>
-          </li>
-          <li className="Box__Nav__Li">
-            <Link to="/Portfolio">PORTFOLIO</Link>
-          </li>
-          <li className="Box__Nav__Li">
-            <Link to="/Education">EDUCACIÓN</Link>
-          </li>
-          <li className="Box__Nav__Li">
-            <Link to="/Skills">HABILIDADES</Link>
-          </li>
-          <li className="Box__Nav__Li">
-            <Link to="/Contact">CONTACTO</Link>
-          </li>
-        </ul>
-      
+      <nav>
+      <ul className="Box__Nav__Ul">
+        <li className="Box__Nav__Li">
+          <Link to="/Natalia" onClick={() => setMenuOpen(false)}>
+            NATALIA
+          </Link>
+        </li>
+        <li className="Box__Nav__Li">
+          <Link to="/AboutMe" onClick={() => setMenuOpen(false)}>
+            SOBRE MI
+          </Link>
+        </li>
+        <li className="Box__Nav__Li">
+          <Link to="/Portfolio">PORTFOLIO</Link>
+        </li>
+        <li className="Box__Nav__Li">
+          <Link to="/Education">EDUCACIÓN</Link>
+        </li>
+        <li className="Box__Nav__Li">
+          <Link to="/Skills">HABILIDADES</Link>
+        </li>
+        <li className="Box__Nav__Li">
+          <Link to="/Contact">CONTACTO</Link>
+        </li>
+      </ul>
+
       {/* Botón de menú hamburguesa para móviles */}
       <div className="Menu_Hamburguesa" onClick={() => setMenuOpen(!menuOpen)}>
         <div className="Bar"></div>
@@ -51,5 +55,6 @@ export default function Nav() {
       </div>
       </nav>
     </div>
+ 
   );
 }
