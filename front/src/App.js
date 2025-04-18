83a5330fb34734b82ed4d85fb5b3bd16b1a92b7d
Natfer84 <natita2804@hotmail.com>
2025-04-18 11:16:01 +0200

    responsive

--- front/src/App.js
+++ front/src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ParticlesBackground from "./components/ParticlesBackground.js"; 
 import Header from "./components/Header.js";
-import Natalia from "./components/Natalia.js";
-import AboutMe from "./components/AboutMe.js";
+import Natalia from "./pages/Natalia.js";
+import AboutMe from "./pages/AboutMe.js";
 //import Nav from "./components/Nav.js";
 
 import "./styles/App.css"; 
