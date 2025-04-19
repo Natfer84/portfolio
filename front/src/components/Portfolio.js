e83af2a08fd5b127fe519265ecf84583dbfc9335
Natfer84 <natita2804@hotmail.com>
2025-04-19 18:29:05 +0200

    portfolio

--- front/src/components/Portfolio.js
+++ front/src/components/Portfolio.js
@@ -5,23 +5,23 @@ export default function Portfolio() {
   const jobs = [
     {
       id: 1,
-      image: "/images/10_yNatalia.jpg",
-      name: "Tribial",
-      descripcion: "Pequeño juego de trivial",
+      image: "/images/halloween.png",
+      name: "Cuestionario de Halloween",
+      descripcion: "Proyecto de Halloween desarrollado exclusivamente con HTML, JavaScript y CSS. El usuario puede seleccionar una categoría y responder un cuestionario de diez preguntas. Por cada respuesta correcta, se incrementa un contador de puntuación. Las preguntas se obtienen dinámicamente desde una API REST.",
       video: " ",
     },
     {
       id: 2,
-      image: "/images/10_yNatalia.jpg",
-      name: "Tienda Pokemon",
-      descripcion: "Tinda de Pokemon",
+      image: "/images/Pokemon.png",
+      name: "Tienda Pokémon",
+      descripcion: "Proyecto realizado con React. Obtiene los Pokémon desde una API REST y permite agregarlos a un carrito de selección. Dentro del carrito, el usuario puede aumentar o disminuir la cantidad de cada Pokémon, o eliminarlos completamente.",
       video: " ",
     },
     {
       id: 3,
-      image: "/images/10_yNatalia.jpg",
+      image: "/images/creanunate.png",
       name: "Tienda Creatividad",
-      descripcion: "Tienda Creatividad",
+      descripcion: "Proyecto final del curso de Desarrollo de Aplicaciones con Tecnología Web, desarrollado con el stack MERN. Se trata de una tienda creativa donde el usuario puede explorar todos los cursos disponibles, así como los próximos lanzamientos. Los usuarios pueden registrarse y acceder a un área personal, donde podrán ver sus cursos favoritos o los que hayan adquirido. Actualmente se encuentra en su versión v1.0.0, con nuevas funcionalidades en desarrollo.",
       video: " ",
     },
   ];
@@ -42,7 +42,7 @@ export default function Portfolio() {
             <h2 className="Container_job_h2">{job.name}</h2>
 
             <p className="Container_job_p">{job.descripcion}</p>
-
+          
             {/*aqui va el enlace*/}
             <div className="Container_job_enlace">
               <a
