8d14090498562e7a09d21454a22661ac75b9dc3b
Natfer84 <natita2804@hotmail.com>
2025-06-01 17:31:06 +0200

    cambiado enlace en el trabajo de halloween en portfolio

--- front/src/components/Portfolio.js
+++ front/src/components/Portfolio.js
@@ -28,9 +28,9 @@ export default function Portfolio() {
       name: "Cuestionario de Halloween",
       descripcion:
         "Proyecto de Halloween desarrollado exclusivamente con HTML, JavaScript y CSS. El usuario puede seleccionar una categoría y responder un cuestionario de diez preguntas. Por cada respuesta correcta, se incrementa un contador de puntuación. Las preguntas se obtienen dinámicamente desde una API REST.",
-      link: "https://tiendapokemania.netlify.app",
-      github: "https://quizhalloweennat.netlify.app/",
-    },
+      link: "https://quizhalloweennat.netlify.app",
+      github: "https://github.com/Natfer84/quiz_halloween.git",
+    }, 
   ];
   return (
     <div className="Container_Porfolio">
